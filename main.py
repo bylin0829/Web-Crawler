2bf34d146dd1f68830fe1ba2833d1d6011e70d37
bylin <bylin0829@gmail.com>
2021-01-24 08:59:15 -0800

    add 2 epub

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 from ebooklib import epub
 
-
 def test2(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     return soup.find_all("div", class_="content")
 
-def exportEpub():
+def exportEpub(txt):
     book = epub.EpubBook()
     # set metadata
     book.set_identifier('id123456')
-    book.set_title('Sample book')
+    book.set_title('Sample book 測試中文哦!')
     book.set_language('en')
 
-    book.add_author('Author Authorowski')
+    book.add_author('DannyLin 丹丹漢堡')
     book.add_author('Danko Bananko', file_as='Gospodin Danko Bananko', role='ill', uid='coauthor')
 
     # create chapter
-    c1 = epub.EpubHtml(title='Intro', file_name='chap_01.xhtml', lang='hr')
-    c1.content=u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
+    c1 = epub.EpubHtml(title='介紹一下', file_name='chap_01.xhtml', lang='hr')
+    # c1.content=u'<h1>Hi Chloe!</h1><p>要不要一起去看五月天?</p>'
+    c1.content=txt
 
     # add chapter
     book.add_item(c1)
@@ -46,8 +46,8 @@ def exportEpub():
     book.spine = ['nav', c1]
 
     # write to the file
-    epub.write_epub('test.epub', book, {})
+    epub.write_epub('test1.epub', book, {})
 
 if __name__=='__main__':
     txt = test2('https://tw.aixdzs.com/read/166/166552/p1.html')
-    exportEpub()
+    exportEpub(txt)
