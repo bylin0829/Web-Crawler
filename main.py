c9287a973e8907c3e94c25477940b9953b9f11cf
bylin <bylin0829@gmail.com>
2021-01-25 08:56:48 -0800

    Create class

--- main.py
+++ main.py
@@ -3,53 +3,77 @@ from bs4 import BeautifulSoup
 from ebooklib import epub
 import unicodedata
 
-def test2(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup.find_all("div", class_="content")
+class HtmlParser:
+    def __init__(self, url):
+        response = requests.get(url)
+        self.soup = BeautifulSoup(response.text, "html.parser")
+    
+    def get_title(self):
+        x = self.soup.find("h1")
+        return str(x)
 
-def exportEpub(txt):
-    book = epub.EpubBook()
-    # set metadata
-    book.set_identifier('id123456')
-    book.set_title('Sample book 測試中文哦!')
-    book.set_language('en')
+    def get_author(self):
+        x = self.soup.find("h2").find("span")
+        return str(x)
 
-    book.add_author('DannyLin 丹丹漢堡')
-    book.add_author('Danko Bananko', file_as='Gospodin Danko Bananko', role='ill', uid='coauthor')
+    def get_content(self):
+        x = self.soup.find("div", class_="content")
+        return str(x)
 
-    # create chapter
-    c1 = epub.EpubHtml(title='介紹一下', file_name='chap_01.xhtml', lang='hr')
-    # c1.content=u'<h1>Hi Chloe!</h1><p>要不要一起去看五月天?</p>'
-    c1.content=txt
+class Epub:
+    def __init__(self, title, author, content, identifier):
+        self.title = title
+        self.author = author
+        self.content = content
+        self.identifier = identifier
+        self.book = epub.EpubBook()
 
-    # add chapter
-    book.add_item(c1)
+    def export_epub(self):
+        # set metadata
+        self.book.set_identifier(self.identifier)
+        self.book.set_title(self.title)
+        self.book.set_language('en')
 
-    # define Table Of Contents
-    book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
-                (epub.Section('Simple book'),
-                (c1, ))
-                )
+        self.book.add_author(self.author)
+        self.book.add_author('Danko Bananko', file_as='Gospodin Danko Bananko', role='ill', uid='coauthor')
 
-    # add default NCX and Nav file
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
+        # create chapter
+        c1 = epub.EpubHtml(title='介紹一下', file_name='chap_01.xhtml', lang='hr')
+        # c1.content=u'<h1>Hi Chloe!</h1><p>要不要一起去看五月天?</p>'
+        c1.content=self.content
 
-    # define CSS style
-    style = 'BODY {color: white;}'
-    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+        # add chapter
+        self.book.add_item(c1)
 
-    # add CSS file
-    book.add_item(nav_css)
+        # define Table Of Contents
+        self.book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
+                    (epub.Section('Simple book'),
+                    (c1, ))
+                    )
 
-    # basic spine
-    book.spine = ['nav', c1]
+        # add default NCX and Nav file
+        self.book.add_item(epub.EpubNcx())
+        self.book.add_item(epub.EpubNav())
 
-    # write to the file
-    epub.write_epub('test_encoder.epub', book, {})
+        # define CSS style
+        style = 'BODY {color: white;}'
+        nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+
+        # add CSS file
+        self.book.add_item(nav_css)
+
+        # basic spine
+        self.book.spine = ['nav', c1]
+
+        # write to the file
+        epub.write_epub('test_encoder.epub', self.book, {})
 
 if __name__=='__main__':
-    txt = test2('https://tw.aixdzs.com/read/166/166552/p1.html')
-    # print(str(txt))
-    exportEpub(str(txt))
+    url = 'https://tw.aixdzs.com/read/271/271523/p42.html'
+    x = HtmlParser(url)
+    y = Epub(title=x.get_title(), author=x.get_author(), content=x.get_content(), identifier='id123456')
+    # print(x.get_author())
+    # print(x.get_title())
+    # print(x.get_content())
+    y.export_epub()
+    # exportEpub(str(txt))
